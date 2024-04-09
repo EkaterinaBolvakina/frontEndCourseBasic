c6fe6d3f0e7b83ff1645dd4b8b7e53e17501af7d
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-04-09 15:09:56 +0200

    commit

--- Lesson08/Code_Lesson/index.js
+++ Lesson08/Code_Lesson/index.js
@@ -1,17 +1,108 @@
-// print info in console:
-// console.log('hello world')
+// вывод в консоль информации
+// console.log("hello world 333")
 
-// variable definieren mit dem Namen s und lege in die Variable hello:
+// создание переменной
 let s = 'hello';
 s = 1;
 
-// Konstante create, Unterschied von Variable: wird einmalig definiert, 
+// создание константы
 const myConst = 111;
-// darf nicht geändert werden, Fehler wird kommen:
 // myConst = 2;
 
-// mit var kann man auch, aber darf man besser nicht !!!
+// создание переменной через var ( так делать не нужно )
 var myVariable = 333;
 
-console.log(s);
-console.log(myConst);
+// console.log(myConst);
+
+// переменная числового типа
+let a = 1;
+
+// переменная строкового типа
+let b = 'hello';
+
+// переменная булевого типа
+let c = true;
+
+// копирование по значению
+let first = 1;
+let second = first;
+
+first = 2;
+
+// console.log(first);
+// console.log(second);
+
+// пустое значение (null)
+let a1 = null;
+
+// console.log(a1);
+
+// переменная со значением undefined ( не определено )
+let a2;
+// console.log(a2);
+
+// псевдокод ветвления
+// if(истина){
+//     // сделай это
+// }else{
+//     // сделай другое
+// }
+
+// let a3 = '10';
+// '10' == 10
+// сравнение с приведением типа
+// if (a3 == 10) {
+//     console.log('a3 = 10');
+// } else {
+//     console.log('не равно');
+// }
+
+// ------------
+// let a3 = '10';
+
+// if (a3 === 10) {
+//     console.log('a3 = 10');
+// } else {
+//     console.log('не равно');
+// }
+
+let a4 = 1;
+let a5 = 2;
+// console.log(a4 + a5);
+
+let res = a4 + a5;
+// console.log(res);
+
+let res2 = 'hello' + 5;
+// console.log(res2);
+
+// сложение строки и числа (конкатенация строк)
+let aa = '2';
+let bb = 3;
+let res3 = aa + bb;
+console.log(res3);
+
+// ------------------
+
+// оператор switch case
+let day = 'monday';
+switch (day) {
+    case 'monday':
+        console.log('понедельник');
+        break;
+    case 'tuesday':
+        console.log('вторинк');
+        break;
+    default:
+        console.log('неизвестный день недели');
+        break;
+}
+
+// оператор ветвление (тоже самое что и switch case - выше )
+if (day === 'monday') {
+    console.log('понедельник');
+} else if (day === 'tuesday') {
+    console.log('вторинк');
+} else {
+    console.log('неизвестный день недели');
+}
