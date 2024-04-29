a9f0bfabb4c57f52c826dcba5f3a13048074bbdb
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-04-29 15:19:28 +0200

    commit

--- Lesson21/Homework/index.js
+++ Lesson21/Homework/index.js
@@ -6,22 +6,22 @@ const delay = (value) => {
             if (typeof value !== 'number') {
                 reject('The argument is not a number');
             } else {
-               resolve(value ** 2); // resolve(value * value);
+                resolve(value ** 2); // resolve(value * value);
             }
         }, 5000);
     });
 }
 
 delay(5)
-.then(data => {
-    console.log(data);
-}).catch(error => {
-    console.log('ERROR 1XX: ', error);
-})
+    .then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.log('ERROR 1XX: ', error);
+    })
 
 delay('5')
-.then(data => {
-    console.log(data);
-}).catch(error => {
-    console.log('ERROR 1XX: ', error);
-})
+    .then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.log('ERROR 1XX: ', error);
+    })
